7734f1d7aaf541aee789485af7087db070f01fa9
yeh05061995 <yeh05061995java>
2025-03-14 16:15:18 +0800

    first commit

---
+++ src/hw1/hw1_4.java
@@ -0,0 +1,20 @@
+package hw1;
+
+public class hw1_4 {
+	
+	public static void main(String[]args) {
+		
+		// 請定義一個常數為3.1415(圓周率)，並計算半徑為5的圓面積與圓周長
+		
+		double  a = 3.1415;
+		
+//		double b = 5*5*3.14;
+		
+		
+		// "半徑為5的圓面積為 ： " +
+		System.out.printf("半徑為5的圓面積為 ： " +  "%.4f \n\n"  ,   25*a );
+		System.out.printf("半徑為5的圓周長為 ： " +  "%.3f "  , 10*a);
+		
+	}
+
+}
